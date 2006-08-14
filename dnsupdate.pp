7fef95ea080dd73fa00214b02fa1ffba2f92d765
davidl <davidl>
2006-08-14 15:08:14 +0000

    add 'make package' target

--- dnsupdate.pp
+++ dnsupdate.pp
@@ -4,12 +4,6 @@
 %set
   name="quest-dnsupdate"
   pp_solaris_name=quest-dnsupdat	# Argh!
-  if test -x ./configure; then
-    version=`./configure --version | head -n 1 | awk '{print $3}'`
-  else
-    version='0.0.0'
-  fi
-
   summary="Updates DNS records for DHCP clients"
 
 %files
@@ -17,24 +11,15 @@
   /opt/quest/man/man8/dnsupdate.8
   /opt/quest/libexec/dnsupdate-install-hooks
 
-%post [rpm]
- /opt/quest/libexec/dnsupdate-install-hooks -i linux
-%preun [rpm]
- /opt/quest/libexec/dnsupdate-install-hooks -r linux
+%set
+[rpm]     install_platform=linux
+[solaris] install_platform=solaris
+[sd]      install_platform=hpux
+[aix]     install_platform=aix
 
-%post [solaris]
- /opt/quest/libexec/dnsupdate-install-hooks -i solaris
-%preun [solaris]
- /opt/quest/libexec/dnsupdate-install-hooks -r solaris
-
-%post [sd]
- /opt/quest/libexec/dnsupdate-install-hooks -i hpux
-%preun [sd]
- /opt/quest/libexec/dnsupdate-install-hooks -r hpux
-
-%post [aix]
- /opt/quest/libexec/dnsupdate-install-hooks -i aix
-%preun [aix]
- /opt/quest/libexec/dnsupdate-install-hooks -r aix
+%post [rpm,solaris,sd,aix]
+ /opt/quest/libexec/dnsupdate-install-hooks -i %{install_platform}
+%preun [rpm,solaris,sd,aix]
+ /opt/quest/libexec/dnsupdate-install-hooks -r %{install_platform}
 
 # vim: ts=2:sw=2:et
